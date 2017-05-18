639624e6f8722f0e7c292d92ab89655a2efe9e49
Rafael Romancini <rafael.romancini@yahoo.com.br>
2017-05-17 21:17:01 -0300

    Tentando inserir dados no banco com o fragmento 1.

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/guifragmentos/Fragmento1Fragment.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/guifragmentos/Fragmento1Fragment.java
@@ -1,8 +1,9 @@
 package br.edu.ifspsaocarlos.sdm.guifragmentos;
 
 
+import android.content.ContentValues;
 import android.content.Context;
-import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -10,12 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Fragmento1Fragment extends Fragment {
+    private DatabaseHelper helper;
+    private EditText nome;
+    private EditText usuario;
+    private EditText senha;
 
     public static Fragmento1Fragment newInstance(Context context) {
         Fragmento1Fragment fragment = new Fragmento1Fragment();
@@ -25,7 +31,7 @@ public class Fragmento1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_fragmento1, null);
+        final View view = inflater.inflate(R.layout.fragment_fragmento1, null);
 
         ((PrincipalActivity)getActivity())
                 .getSupportActionBar()
@@ -37,7 +43,14 @@ public class Fragmento1Fragment extends Fragment {
             public void onClick(View v){
                 Fragmento2Fragment f2 = new Fragmento2Fragment();
                 Bundle args = new Bundle();
-                args.putString("id", "1");
+                nome = (EditText) view.findViewById(R.id.et_nome);
+                usuario = (EditText) view.findViewById(R.id.et_usuario);
+                senha = (EditText) view.findViewById(R.id.et_senha);
+                long id = salvarUsuario(
+                        nome.getText().toString(),
+                        usuario.getText().toString(),
+                        senha.getText().toString());
+                args.putString("id", Long.toString(id));
                 f2.setArguments(args);
                 final FragmentTransaction ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.content_frame, f2);
@@ -48,4 +61,22 @@ public class Fragmento1Fragment extends Fragment {
         return view;
     }
 
+    private long salvarUsuario(String nome, String usuario, String senha){
+        long resultado = 0;
+        try {
+            SQLiteDatabase db = helper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("nome_completo", nome);
+            values.put("usuario", usuario);
+            values.put("senha", senha);
+            resultado = db.insert("usuarios", null, values);
+        } catch (Exception e) {
+            System.out.println("erro: " + e);
+            System.out.println("nome: " + nome);
+            System.out.println("usuario: " + usuario);
+            System.out.println("senha: " + senha);
+        }
+
+        return resultado;
+    }
 }
--- app/src/main/java/br/edu/ifspsaocarlos/sdm/guifragmentos/Fragmento2Fragment.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/guifragmentos/Fragmento2Fragment.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,5 +36,4 @@ public class Fragmento2Fragment extends Fragment {
 
         return view;
     }
-
 }
